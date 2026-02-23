beb7ad6260172646f21b87e67f8f72ef0de47ab7
Marco Mosqueda <mam785@txstate.edu>
2026-02-23 01:45:45 -0600

    SCRUM-43 Finished adding the rest of the code for the 'done' filtering, so that any assignments not finished will be stored in a new list

--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -14,12 +14,17 @@ export interface ScheduledPlan // SCRUM-46 interface to know what properties the
   TaskOverloaded: boolean; // if the total time required is greater than the available time
 }
 
-export const PlanGenerator 
-= ( // function to generate the plan
+export const PlanGenerator = ( // function to generate the plan
   assignments: Assignment[],  // array of assignments
   AvailableTime: number // available time in minutes
 ): ScheduledPlan => { // return the plan
   const currentDate = new Date(); // current date
+
+// Goal is to ensure that completed work disappears from your calculations
+// Scrum-43 work below
+
+ const activeAssignments = assignments.filter(a => a.status !== 'done'); // filter out completed assignments
+
 
  // Add a filter to score the assignments to know their priority 
  const sortedAssignments = [...assignments].sort((a, b) => { // sort by due date, earlier due dates get higher priority 
@@ -27,18 +32,18 @@ export const PlanGenerator
                                                                          // shorter duration gets higher priority
   });
 
+ const totalTime = activeAssignments.reduce((acc, curr) => acc + curr.duration_inMinutess, 0); // calculate total time required to complete all active assignments
+
 
 // Plan whenever something changes, such as a student finishing a task early or updating their study hours.
 // Scrum-38 work below
 
 
-// Goal is to ensure that completed work disappears from your calculations
-// Scrum-43 work below
 
- const activeAssignments = assignments.filter(a => a.status !== 'done'); // filter out completed assignments
  
   return {
     // return the sorted assignments 
+    sortedAssignments
     Time_ToComplete: totalTime, // return the total time required
     TaskOverloaded: totalTime > AvailableTime // return if the user is overloaded or not
   };
