a812094df1cdbac1f6ceaacc15ec5b284ac142bf
Marco Mosqueda <mam785@txstate.edu>
2026-03-12 01:26:06 -0500

    SCRUM-66 fix import from mongodb assignment- schedule plan, and import

--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -1,22 +1,16 @@
-export interface Assignments // SCRUM-46 interface to know what properties the object should have
-{
-  id: number; // id of the assignment/hw
-  title: string; // title of the assignment/hw
-  duration_inMinutess: number; // duration in minutes to complete the assignment
-  dueDate: Date; // due date for assignments
-  minuteSpent : number;// This will help us track how much work is done
-  priority: 'low' | 'medium' | 'IMMEDIATE'; // status of the assignment to let the user know if it's completed or not
-  completed: boolean;
-}
+import Assingment from '@/mongodb-mongoose/model/Assignment';
+
+type AssignmentDocument = InstanceType<type of Assignment>;
+
 
 export interface ScheduledPlan // SCRUM-46 interface to know what properties the object should have
 {
-  sortedAssignments: Assignments[]; // assignments sorted by priority
+  sortedAssignments: any[]; // assignments sorted by priority
   Time_ToComplete: number; // total time required
   TaskOverloaded: boolean; // if the total time required is greater than the available time
 }
 
-export const PlanGenerator = (assignments: Assignments[], AvailableTime: number): ScheduledPlan => {
+export const PlanGenerator = (assignments: AssignmentDocument[], AvailableTime: number): ScheduledPlan => {
 
 // Goal is to ensure that completed work disappears from your calculations
 // Scrum-43 work below
@@ -26,7 +20,7 @@ export const PlanGenerator = (assignments: Assignments[], AvailableTime: number)
 // Add a filter to score the assignments to know their priority 
  // Plan whenever something changes, such as a student finishing a task early or updating their study hours.
  // Filter to score the assignments to know their priority 
- // Scrum-38 work below 
+ // Scrum-38 work below
  
 const sortedAssignments = [...activeAssignments].sort((a, b) => { // sort by due date, earlier due dates get higher priority 
     if (a.priority === 'IMMEDIATE' && b.priority !== 'IMMEDIATE') return -1; // if a is in progress/immediate but b is not then put a first
