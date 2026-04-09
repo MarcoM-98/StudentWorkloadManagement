285abb4fd09756c346b737cbd42a0f60c75a2520
Marco Mosqueda <mam785@txstate.edu>
2026-04-09 02:54:29 -0500

    SCRUM-99 fixed mongodb connectivity issues while autosaving/saving a file and fix some variable names to comply with Next.js newest versions

--- src/app/api/assignments/[id]/route.ts
+++ src/app/api/assignments/[id]/route.ts
@@ -1,15 +1,14 @@
 import { NextResponse } from 'next/server'; // send data back to the browser
-import dbConnect from '@/mongodb-mongoose/dbConnect';
-import Assignment from '@/mongodb-mongoose/model/Assignment';
-
+import { connectDB } from "../../../../../mongodb-mongoose/db.js"; 
+import Assignment from "../../../../../mongodb-mongoose/model/Assignment.js";
 export async function PATCH( // will only run when you send a patch request
                             // allows you to modify specific fields of a data object without requiring or replacing the entire resource such as only specific data
   request: Request, // has the data being sent from AssignmentCard
-  { params }: { params: { id: string } } // next.js grabs the ID from the url and puts it here to know which card to update
+  { params }: { params: Promise< { id: string }> } // next.js grabs the ID from the url and puts it here to know which card to update
 ) {
 try {
-    await dbConnect(); // Ensure we are connected
-    const { id } = params; // Destructures the specific assignment ID from the URL parameters
+    await connectDB(); // Ensure we are connected
+    const { id } = await params; // Destructures the specific assignment ID from the URL parameters
     const body = await request.json(); // Parses the incoming "envelope" of data into a readable JavaScript object (the editData)
 
     
